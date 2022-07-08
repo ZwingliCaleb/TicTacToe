fbf1f67b4d7427be036abcfd0f5b53674c62b301
ZwingliCaleb <calebzwingli@gmail.com>
2022-07-08 19:44:33 +0300

    6th

--- script.js
+++ script.js
@@ -1,96 +1,114 @@
-//slecting al required elements
-const selectBox = document.querySelector(".selectionbox"),
-selectXBtn = selectBox.queySelector(".playerX"),
-selectOBtn = selectBox.querySelector(".playerO"),
+const selectBox = document.querySelector(".select-box"),
+selectBtnX = selectBox.querySelector(".options .playerX"),
+selectBtnO = selectBox.querySelector(".options .playerO"),
 playBoard = document.querySelector(".play-board"),
+players = document.querySelector(".players"),
 allBox = document.querySelectorAll("section span"),
 resultBox = document.querySelector(".result-box"),
 wonText = resultBox.querySelector(".won-text"),
 replayBtn = resultBox.querySelector("button");
 
-window.onload = () =>{ //once window is loaded
-    for (let i = 0; i< allBox.length; i++){ //add onClick attribute in all available section's spans
-        allBox[i].setAttribute("onClick","clickedBox(this)");
-    }
-
-    selectXBtn.onClick =() =>{
-        selectBox.classList.add("hide"); //hide the select box on playerX button clicked
-        playBoard.classList.add("show"); //show the playboard section on playerX button clicked
-    }
-    selectOBtn.onClick = () =>{
-        selectBox.classList.add("hide");//hide the select box on playerO button clicked
-        playBoard.classList.add("show");//show the playboard selection on playerO button clicked
-        players.setAttribute("class", "players active player");
+window.onload = ()=>{
+    for (let i = 0; i < allBox.length; i++) {
+       allBox[i].setAttribute("onclick", "clickedBox(this)");
     }
 }
 
-let playerXIcon = "fas fa-times",//class name of fontawesome cross icon
-let playerOIcon = "fas fa-circle",//class name for fontawesome circle icon
+selectBtnX.onclick = ()=>{
+    selectBox.classList.add("hide");
+    playBoard.classList.add("show");
+}
+
+selectBtnO.onclick = ()=>{ 
+    selectBox.classList.add("hide");
+    playBoard.classList.add("show");
+    players.setAttribute("class", "players active player");
+}
+
+let playerXIcon = "fas fa-times",
+playerOIcon = "far fa-circle",
 playerSign = "X",
 runBot = true;
 
-//user click function
 function clickedBox(element){
-    if(players.classList.contains("")){
-        playerSign = "0";
-        element.innerHTML = '<i class = "${playerOIcon}"></i>';//adding circle icon tag inside user clicked element.
-        players.classList.add("active");
+    if(players.classList.contains("player")){
+        playerSign = "O";
+        element.innerHTML = `<i class="${playerOIcon}"></i>`;
+        players.classList.remove("active");
         element.setAttribute("id", playerSign);
     }else{
-        element.innerHTML = '<i class="${playerXIcon}"></i>';//adding cross icon tag inside user clicked element.
+        element.innerHTML = `<i class="${playerXIcon}"></i>`;
         element.setAttribute("id", playerSign);
         players.classList.add("active");
     }
     selectWinner();
-    element.style.pointerEvents = "none"; //once user selects any box then that box can't be selected again.
+    element.style.pointerEvents = "none";
     playBoard.style.pointerEvents = "none";
-    let randomTimeDelay = ((Math.random() * 1000) +200).toFixed();
+    let randomTimeDelay = ((Math.random() * 1000) + 200).toFixed();
     setTimeout(()=>{
         bot(runBot);
     }, randomTimeDelay);
 }
 
-//bot click function
 function bot(){
-    let array = [];//creating an empty array....we'll store unselected box index in this array
+    let array = [];
     if(runBot){
         playerSign = "O";
-        for (let i = 0; i < allBox.length; i++){
+        for (let i = 0; i < allBox.length; i++) {
             if(allBox[i].childElementCount == 0){
                 array.push(i);
             }
         }
-    }
-    for (let i = 0; i< allBox.length; i++){
-        if(allBox[i].childElementCount == 0){//if span has no chid element
-            array.push(i);//inserting unclicked or unselected boxes inside array means thatspan has no children
-            console.log(i + "" + "has no children");
+        let randomBox = array[Math.floor(Math.random() * array.length)];
+        if(array.length > 0){
+            if(players.classList.contains("player")){ 
+                playerSign = "X";
+                allBox[randomBox].innerHTML = `<i class="${playerXIcon}"></i>`;
+                allBox[randomBox].setAttribute("id", playerSign);
+                players.classList.add("active");
+            }else{
+                allBox[randomBox].innerHTML = `<i class="${playerOIcon}"></i>`;
+                players.classList.remove("active");
+                allBox[randomBox].setAttribute("id", playerSign);
+            }
+            selectWinner();
         }
-    }
-    let randomBox = array[Math.floor(Math.random() * array.length)];
-    if(players.classList.contains("player")){
+        allBox[randomBox].style.pointerEvents = "none";
+        playBoard.style.pointerEvents = "auto";
         playerSign = "X";
-        allBox[randomBox].innerHTML = `<i class = "${playerXIcon}"></i>`;
-        allBox[randomBox].setAttribute("id", playerSign);
-        players.classList.add("active");
-    }else{
-        allBox[randomBox].innerHTML = `<i class="${playerOIcon}"></i>`;
-        players.classList.remove("active");
-        allBox[randomBox].setAttribute("id", playerSign);
     }
-    selectWinner();
-}
-allBox[randomBox].style.pointerEvents = "none";
-playBoard.style.pointerEvents = "auto";
-playerSign = "X";
-}
 }
 
-function getidVal (classname){
+function getIdVal(classname){
     return document.querySelector(".box" + classname).id;
 }
-function checkIdSign(val1, val2, val3, sign){
-    if(getidVal(val1) == sign && getIdVal (val2) == sign && getIdVal(val3) == sign){
+function checkIdSign(val1, val2, val3, sign){ 
+    if(getIdVal(val1) == sign && getIdVal(val2) == sign && getIdVal(val3) == sign){
         return true;
     }
+}
+function selectWinner(){
+    if(checkIdSign(1,2,3,playerSign) || checkIdSign(4,5,6, playerSign) || checkIdSign(7,8,9, playerSign) || checkIdSign(1,4,7, playerSign) || checkIdSign(2,5,8, playerSign) || checkIdSign(3,6,9, playerSign) || checkIdSign(1,5,9, playerSign) || checkIdSign(3,5,7, playerSign)){
+        runBot = false;
+        bot(runBot);
+        setTimeout(()=>{
+            resultBox.classList.add("show");
+            playBoard.classList.remove("show");
+        }, 700);
+        wonText.innerHTML = `Player <p>${playerSign}</p> won the game!`;
+    }else{
+        if(getIdVal(1) != "" && getIdVal(2) != "" && getIdVal(3) != "" && getIdVal(4) != "" && getIdVal(5) != "" && getIdVal(6) != "" && getIdVal(7) != "" && getIdVal(8) != "" && getIdVal(9) != ""){
+            runBot = false;
+            bot(runBot);
+            setTimeout(()=>{
+                resultBox.classList.add("show");
+                playBoard.classList.remove("show");
+            }, 700);
+            wonText.textContent = "Match has been drawn!";
+        }
+    }
+}
+
+replayBtn.onclick = ()=>{
+    window.location.reload();
 }
